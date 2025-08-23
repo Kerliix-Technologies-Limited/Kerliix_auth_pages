6c82123c48a68c7dc5100ec6dfaea18e76188999
Mahmood Kaliika <mahmoodkaliika810@gmail.com>
2025-08-23 08:33:30 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://kerliix-auth-server-ojud.onrender.com/api',
   withCredentials: true,
 });
 
 export default API;
+
